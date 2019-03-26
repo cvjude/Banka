30b8e4805a63545f0b744402ad9e041aebe12f87
cvjude <jjchinosoviolet@gmail.com>
2019-03-26 20:23:05 +0100

    feat(api): creates an api endpoint to signup a user [Finishes #164909771]

--- Server/helper/Utilities.js
+++ Server/helper/Utilities.js
@@ -1,7 +1,43 @@
 /* eslint-disable linebreak-style */
+import Joi from 'joi';
 
 class Util {
+  /**
+  * @static
+  * @description Validates an Object against the schema
+  * @param {Object} validateObject - object to be validated
+  * @param {Object} schemadata - Joi schema
+  * @memberof Controllers
+  */
 
+  static validateJoi(validateObject, schemadata) {
+    let error;
+    Joi.validate(validateObject, schemadata, { convert: false }, (err) => {
+      if (err) {
+        error = err.details[0].message;
+        error = error.replace(/"/gi, '');
+        if (error.includes('[A-Za-z]')) { error = `${err.details[0].path[0]} should be a string`; }
+      }
+    });
+    return error;
+  }
+
+
+  /**
+  * @static
+  * @description Returns message based on the status
+  * @param {Object} res - Response object
+  * @param {integer} statusCode - status code to be sent
+  * @param {Object} schemadata - the appropraite error message
+  * @memberof Controllers
+  */
+
+  static errorstatus(res, statusCode, errormessage) {
+    return res.status(statusCode).json({
+      status: statusCode,
+      error: errormessage,
+    });
+  }
 }
 
 export default Util;
--- Server/router/router.js
+++ Server/router/router.js
@@ -1,10 +1,14 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
 import controller from '../controllers/controller';
+import Validate from '../middleware/validator';
+import User from '../controllers/usercontroller';
 
 // the sub app using url versioning
 const router = express();
 
 router.get('/', controller.welcome);
+
+router.post('/auth/signup', Validate.Signup, User.signup);
 
 export default router;
