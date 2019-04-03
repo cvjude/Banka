295e0dd3dfdf1e14698bc1652e74f1389c056090
cvjude <jjchinosoviolet@gmail.com>
2019-04-03 16:35:18 +0100

    feat(validate): add validation to forms
    -Add validation to all forms on the page
    -Add pop up for incorrect fields
    
    [Delivers
    #165091629]

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -19,6 +19,22 @@ btn[0].addEventListener('click', (event) => {
     // }, 0.5);
 })
 
+accountForm = document.querySelector('.accountform');
+error = document.querySelector('.error');
+
+accountForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let input = accountForm.querySelector('input');
+      if(input.className === 'valid' && input.value < 999.99){
+        error.style.display = 'block';
+      }
+      else {
+        dialog.close();
+        input.value = '';
+        error.style.display = 'none';
+      }
+});
+
 function drawOnCanvas(filein) {
     let file = null;
     if(filein.type.match(/^image\//))
