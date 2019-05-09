d7c3b6c6c5139f679847fa86c6d15359513f95c9
cvjude <jjchinosoviolet@gmail.com>
2019-05-09 16:39:22 +0100

    Deploy UI to gh-pages

--- server/helper/passwordHash.js
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const hash = password => bcrypt.hashSync(password, 6);
-const checkPassword = (password, passwordHash) => bcrypt.compareSync(password, passwordHash);
-export { hash, checkPassword };
